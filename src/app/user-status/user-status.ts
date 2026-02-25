057ae2d6b582667fa2e9dc1dd6846b560168a7dc
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-02-25 11:43:01 -0500

    Add user-card, user-status and user-bio components

---
+++ src/app/user-status/user-status.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-status',
+  imports: [],
+  templateUrl: './user-status.html',
+  styleUrl: './user-status.css',
+})
+export class UserStatus {
+
+}

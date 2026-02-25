8b097a29d2bebaf580d5eec2159142e30e7505cf
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2026-02-25 11:49:32 -0500

    Add User model interface pwd clear

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+
+
+// User interface used across the dashboard components
+export interface User {
+  name: string;
+  age: number;
+  profilePicture: string;
+  bio: string;
+  isOnline: boolean;
+}
